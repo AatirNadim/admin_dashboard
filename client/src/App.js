d40740ffd3c7f55c56254df6c7d02f605214a58f
Aatir Nadim <aatir.nadim@gmail.com>
2023-03-25 23:43:13 +0530

    working on the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
+// import { Dashboard } from "@mui/icons-material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
 function App() {
+  // grabbing the state from the global mode
+  const mode = useSelector((state) => {
+    return state.global.mode;
+  })
+  // maybe problem here
+  // passing the state to the createTheme, invoked whenever there is change in mode
+  const theme = useMemo(() =>
+    createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="App">
-      
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme} >
+          <CssBaseline />
+          <Routes>
+            <Route  element = {<Layout />}>
+              <Route path = "/" element = {<Navigate to={'/dashboard'} replace/>}/>
+              <Route path = "/dashboard" element = {<Dashboard />}/>
+            </Route>
+
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
