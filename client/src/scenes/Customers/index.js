1f5a458d3fd5372fc5f569ad4d267eb74a7b1558
Aatir Nadim <aatir.nadim@gmail.com>
2023-04-01 19:57:51 +0530

    custom styling for the datagrid

--- client/src/scenes/Customers/index.js
+++ client/src/scenes/Customers/index.js
@@ -54,7 +54,31 @@ const Customers = () => {
     <Box m='1.5rem 2.5rem' >
       <Header title='CUSTOMERS' subTitle='List of Customers' />
       <Box mt='40px' height='75vh' 
-      
+        sx = {{
+          '& .MuiDataGrid-root' : {
+            border :'none',
+          },
+          '& .MuiDataGrid-cell' : {
+            borderBottom : 'none',
+          },
+          '& .MuiDataGrid-columnHeaders' : {
+            backgroundColor : theme.palette.background.alt,
+            color : theme.palette.secondary[100],
+            borderBottom : 'none',
+          },
+          '& .MuiDataGrid-virtualScoller' : {
+            backgroundColor : theme.palette.primary.light,
+          },
+          '& .MuiDataGrid-footerContainer' :  {
+            backgroundColor : theme.palette.background.alt,
+            color : theme.palette.secondary[100],
+            borderTop : 'none',
+          },
+          '& .MuiDataGrid-toolbarContainer .MuiButton-text' : {
+            color : `${theme.palette.secondary[200]} !important`,
+          }
+
+        }}
       
       
       >
