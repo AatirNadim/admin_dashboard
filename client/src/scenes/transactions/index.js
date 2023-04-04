3a94840f4190197fb342cdaf504708798f8a7c95
Aatir Nadim <aatir.nadim@gmail.com>
2023-04-05 00:39:00 +0530

    transactions grid

--- client/src/scenes/transactions/index.js
+++ client/src/scenes/transactions/index.js
@@ -57,7 +57,43 @@ const Transactions = () => {
   ];
 
   return (
-    <div>Transactions</div>
+    <Box m='1.5rem 2.5rem' >
+      <Header title={'TRANSACTIONS'} subTitle={'List of transactions'} />
+      <Box  height='80vh' 
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid  
+          loading = {isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || [] }
+          columns={columns}
+          
+        />
+      </Box>
+    </Box>
   )
 }
 
